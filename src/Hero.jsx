b537a12974dba948e5236d9bc742e8cc4c8a7e96
SarzhanIhor <75244246+SarzhanIhor@users.noreply.github.com>
2024-07-08 19:07:35 +0200

    commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
     <section className='hero'>
       <div className="hero-center">
         <div className="hero-title">
-          <h1>Ihor Sarzhan</h1>
-          <p>Hello. My name is Ihor. I'm a front-end developer. I write my code using JS, React... I use different libraries and am always ready to learn new things. Ready for new acquaintances. </p>
+          <h1>My Projects</h1>
+          <p>Hello. My name is Ihor Sarzhan. I'm a front-end developer. I write my code using JS, React... I use different libraries and am always ready to learn new things. Ready for new acquaintances. </p>
         </div>
         <div className="img-container">
           <img src={heroimg} alt="Web" className='img'/>
